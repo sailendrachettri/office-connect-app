b063f0739f5e78b8f233e3ca97a4654bc0b6e7b1
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-12-11 23:05:36 +0530

    latest boilet template

--- office-connect-client/src/renderer/src/App.jsx
+++ office-connect-client/src/renderer/src/App.jsx
@@ -1,9 +1,7 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+
 import Home from './components/public/home/Home'
 
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 
   return (
     <>
--- office-connect-client/src/renderer/src/components/public/home/Home.jsx
+++ office-connect-client/src/renderer/src/components/public/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='text-3xl text-pink-500'>Home</div>
   )
 }
 
