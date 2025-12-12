e71e658e495f3ab6eab4b906c0c29a9c6ab73e8c
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-12-12 07:58:36 +0530

    template is ready

--- office-connect-client/src/renderer/src/components/public/home/Home.jsx
+++ office-connect-client/src/renderer/src/components/public/home/Home.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import Menu from '../menu/Menu'
+import Sidebar from '../sidebars/Sidebar'
+import Headers from '../headers/Headers'
+import Landing from '../landing-page/Landing'
 
 const Home = () => {
   return (
-    <div className='text-3xl text-pink-500'>Home</div>
+    <>
+      <div className="w-screen h-screen overflow-hidden bg-gray-100 flex">
+        {/* LEFT VERTICAL MENU */}
+        <div className="w-[70px] bg-gray-900 text-white flex flex-col items-center py-4 gap-6">
+          <Menu />
+        </div>
+
+        {/* LEFT SIDEBAR (CHAT LIST) */}
+        <div className="w-[320px] bg-white border-r border-gray-200">
+          <Sidebar />
+        </div>
+
+        {/* RIGHT MAIN CHAT AREA */}
+        <div className="flex-1 flex flex-col">
+          {/* TOP HEADER */}
+          <div className="h-[70px] bg-white border-b border-gray-200 shadow-sm px-4 flex items-center">
+            <Headers />
+          </div>
+
+          {/* CHAT BODY */}
+          <div className="flex-1 overflow-y-auto bg-gray-50">
+            <Landing />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
---
+++ office-connect-client/src/renderer/src/components/public/sidebars/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <>
+    
+    Office Connect
+
+    <div className='text-slate-900'>
+        Search here
+    </div>
+
+    <div>
+        Friends listing
+    </div>
+    </>
+  )
+}
+
+export default Sidebar
