8a8068cdd209a47c047e28f379798a29fdbdff60
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-12-13 08:48:04 +0530

    UI improvements - tab added , default message area added

--- office-connect-client/src/renderer/src/components/public/home/Home.jsx
+++ office-connect-client/src/renderer/src/components/public/home/Home.jsx
@@ -5,56 +5,99 @@ import Headers from '../headers/Headers'
 import Landing from '../landing-page/Landing'
 import UserRegister from '../../common/UserRegister'
 import LoginUser from '../../common/LoginUser'
+import UserProfile from '../../common/UserProfile'
 
 const Home = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true)
   const [showLogin, setShowLogin] = useState(true)
-  const [selectedUsersProfileId, setSelectedUsersProfileId] = useState(null);
+  const [selectedUsersProfileId, setSelectedUsersProfileId] = useState(null)
+  const [selectedTab, setSelectedTab] = useState('chat')
 
   return (
     <>
-    {/* Icons to minimize, fullscreen and close */}
+      {/* Icons to minimize, fullscreen and close */}
       <div className="w-full h-7 bg-slate-200 text-slate-700 flex items-center px-4 select-none drag-region">
         {/* <div className="text-sm font-medium">Office Connect</div> */}
 
         <div className="ml-auto flex gap-3 no-drag">
-          <button onClick={() => window.api.minimize()} className="px-3 cursor-pointer hover:bg-slate-50 rounded-sm">
+          <button
+            onClick={() => window.api.minimize()}
+            className="px-3 cursor-pointer hover:bg-slate-50 rounded-sm"
+          >
             —
           </button>
-          <button onClick={() => window.api.maximize()} className="px-3 cursor-pointer hover:bg-slate-50 rounded-sm">
+          <button
+            onClick={() => window.api.maximize()}
+            className="px-3 cursor-pointer hover:bg-slate-50 rounded-sm"
+          >
             ▢
           </button>
-          <button onClick={() => window.api.close()} className="px-3 hover:bg-red-600 hover:text-slate-300 cursor-pointer rounded-sm">
+          <button
+            onClick={() => window.api.close()}
+            className="px-3 hover:bg-red-600 hover:text-slate-300 cursor-pointer rounded-sm"
+          >
             ✕
           </button>
         </div>
       </div>
 
       {isLoggedIn ? (
-        <div className="w-screen h-screen overflow-hidden bg-gray-100 flex">
-          {/* LEFT VERTICAL MENU */}
-          <div className="w-[70px] bg-slate-50 border border-slate-200 flex flex-col items-center py-4 gap-6 p-1">
-            <Menu setShowLogin={setShowLogin} setIsLoggedIn={setIsLoggedIn} />
+        <section className="w-screen h-screen overflow-hidden bg-gray-100 flex">
+          {/* LEFT VERTICAL MENU — COMMON */}
+          <div className="w-[70px] bg-slate-50 border-r border-slate-200 flex flex-col items-center py-4 gap-6">
+            <Menu
+              setShowLogin={setShowLogin}
+              setIsLoggedIn={setIsLoggedIn}
+              selectedTab={selectedTab}
+              setSelectedTab={setSelectedTab}
+            />
           </div>
 
-          {/* LEFT SIDEBAR (CHAT LIST) */}
-          <div className="w-[420px] bg-white border-r border-slate-200 p-1">
-            <Sidebar setShowLogin={setShowLogin} setIsLoggedIn={setIsLoggedIn} setSelectedUsersProfileId={setSelectedUsersProfileId} />
-          </div>
+          {/* RIGHT SIDE — CHANGES BASED ON TAB */}
+          <div className="flex-1 flex">
+            {/*  CHAT TAB  */}
+            {selectedTab === 'chat' && (
+              <>
+                {/* CHAT LIST */}
+                <div className="w-[420px] bg-white border-r border-slate-200 p-1">
+                  <Sidebar
+                    setShowLogin={setShowLogin}
+                    setIsLoggedIn={setIsLoggedIn}
+                    setSelectedUsersProfileId={setSelectedUsersProfileId}
+                  />
+                </div>
 
-          {/* RIGHT MAIN CHAT AREA */}
-          <div className="flex-1 flex flex-col">
-            {/* TOP HEADER */}
-            <div className="h-[70px] bg-white border-b border-slate-200 shadow-sm px-4 flex items-center p-1">
-              <Headers />
-            </div>
+                {/* CHAT AREA */}
+                <div className="flex-1 flex flex-col">
+                  {/* HEADER */}
+                  <div className="h-[70px] bg-white border-b border-slate-200 shadow-sm px-4 flex items-center">
+                    <Headers />
+                  </div>
 
-            {/* CHAT BODY */}
-            <div className="flex-1 max-h-[88vh] overflow-y-auto ">
-              <Landing selectedUsersProfileId={selectedUsersProfileId} />
-            </div>
+                  {/* BODY */}
+                  <div className="flex-1 overflow-y-auto">
+                    <Landing selectedUsersProfileId={selectedUsersProfileId} />
+                  </div>
+                </div>
+              </>
+            )}
+
+            {/*  PROFILE TAB  */}
+            {selectedTab === 'profile' && (
+              <div className="flex-1 flex flex-col">
+                {/* HEADER */}
+                <div className="h-[70px] bg-white border-b border-slate-200 shadow-sm px-4 flex items-center">
+                  Profile
+                </div>
+
+                {/* BODY */}
+                <div className="flex-1 overflow-y-auto bg-white">
+                  <UserProfile />
+                </div>
+              </div>
+            )}
           </div>
-        </div>
+        </section>
       ) : (
         <>
           {showLogin ? (
--- office-connect-client/src/renderer/src/components/public/menu/Menu.jsx
+++ office-connect-client/src/renderer/src/components/public/menu/Menu.jsx
@@ -2,28 +2,40 @@ import { useState } from 'react'
 import { BsChatLeftDots } from 'react-icons/bs'
 import { IoSettingsOutline } from 'react-icons/io5'
 import { IoPowerOutline } from 'react-icons/io5'
+import { FaRegUser } from 'react-icons/fa6'
 
-const Menu = ({setShowLogin, setIsLoggedIn}) => {
-  const [open, setOpen] = useState(false);
+const Menu = ({ setShowLogin, setIsLoggedIn, selectedTab, setSelectedTab }) => {
+  const [open, setOpen] = useState(false)
 
-  const handleLogout = ()=>{
-    setShowLogin(true);
-    setIsLoggedIn(false);
+  const handleLogout = () => {
+    setShowLogin(true)
+    setIsLoggedIn(false)
   }
+
+  console.log(selectedTab)
 
   return (
     <div className="fixed left-1 top-10 bottom-5 flex flex-col justify-between">
-      {/* Chat icon (top-left) */}
-      <div className="rounded-full bg-slate-100 p-4 text-slate-600 cursor-pointer hover:bg-slate-200 transition">
-        <BsChatLeftDots size={22} />
+      <div>
+        {/* Chats */}
+        <div onClick={()=>{setSelectedTab('chat')}} className={`${selectedTab == 'chat' ? 'bg-ternary' : 'bg-slate-100'} rounded-full  p-4 text-slate-600 cursor-pointer hover:bg-ternary transition`}>
+          <BsChatLeftDots size={22} />
+        </div>
+
+        {/* User profile */}
+        <div onClick={()=>{setSelectedTab('profile')}}
+          className={`rounded-full  ${selectedTab == 'profile' ? ' bg-ternary' : 'bg-slate-100'} p-4 mt-6  text-slate-600 cursor-pointer hover:bg-ternary transition`}
+        >
+          <FaRegUser size={22} />
+        </div>
       </div>
 
       {/* Bottom-left section (settings + popup modal) */}
       <div className="relative">
         {/* Settings icon */}
         <div
-          onClick={() => setOpen(!open)}
-          className="rounded-full bg-slate-100 p-4 text-slate-600 cursor-pointer hover:bg-slate-200 transition"
+          onClick={() => {setOpen(!open); setSelectedTab('settings')}}
+          className={`rounded-full  ${selectedTab == 'settings' ? ' bg-ternary' : 'bg-slate-100'} p-4 mt-6  text-slate-600 cursor-pointer hover:bg-ternary transition`}
         >
           <IoSettingsOutline size={22} />
         </div>
@@ -32,7 +44,10 @@ const Menu = ({setShowLogin, setIsLoggedIn}) => {
         {open && (
           <div className="absolute bottom-16 left-3 bg-white shadow-lg border border-slate-200 rounded-lg w-40 p-2">
             <ul className="text-slate-700">
-              <li onClick={()=> handleLogout()} className="p-2 hover:bg-slate-100 rounded cursor-pointer text-red-500 flex items-center  gap-x-1 flex-nowrap">
+              <li
+                onClick={() => handleLogout()}
+                className="p-2 hover:bg-slate-100 rounded cursor-pointer text-red-500 flex items-center  gap-x-1 flex-nowrap"
+              >
                 <IoPowerOutline size={20} /> Logout
               </li>
             </ul>
