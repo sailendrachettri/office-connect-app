4ac0e6ad234544d24ca79f6cc056e898aef087a1
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-12-25 12:52:04 +0530

    electron build

--- office-connect-client/src/main/index.js
+++ office-connect-client/src/main/index.js
@@ -98,7 +98,7 @@ function createWindow() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('office.connect')
 
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
